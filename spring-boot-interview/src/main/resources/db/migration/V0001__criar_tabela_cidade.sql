572bebc15d4c40cd24f9ad121f8430acb2aa0d19
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2021-01-19 12:45:09 -0300

    Configuração Banco de Dados Postgres, e implementação da migration para criar a tabela cidade através do FlyWay.

---
+++ spring-boot-interview/src/main/resources/db/migration/V0001__criar_tabela_cidade.sql
@@ -0,0 +1,13 @@
+CREATE TABLE public.cidade
+(
+    id SERIAL,
+    nome character varying(100) COLLATE pg_catalog."default",
+    estado character varying(2) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT endereco_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+  
+INSERT INTO public.cidade(nome, estado) VALUES('Blumenau','SC');
+INSERT INTO public.cidade(nome, estado) VALUES('Passo Fundo','RS');
+INSERT INTO public.cidade(nome, estado) VALUES('Curitiba','PR');
