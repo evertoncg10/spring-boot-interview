ee6bccd0db496b1c319df64e18923eefcca37207
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2021-01-19 11:55:17 -0300

    Criação do projeto utilizando Spring Boot.

---
+++ spring-boot-interview/src/main/java/br/com/compasso/springbootinterview/api/SpringBootInterviewApplication.java
@@ -0,0 +1,13 @@
+package br.com.compasso.springbootinterview.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInterviewApplication.class, args);
+	}
+
+}
---
+++ spring-boot-interview/src/test/java/br/com/compasso/springbootinterview/api/SpringBootInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compasso.springbootinterview.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
