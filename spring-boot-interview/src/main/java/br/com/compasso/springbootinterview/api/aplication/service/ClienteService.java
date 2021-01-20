4fa498092d79bc370377a344b502f9ee05d27dec
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2021-01-19 21:13:08 -0300

    Implementação do end point de salvar cliente.

--- spring-boot-interview/src/main/java/br/com/compasso/springbootinterview/api/aplication/service/ClienteService.java
+++ spring-boot-interview/src/main/java/br/com/compasso/springbootinterview/api/aplication/service/ClienteService.java
@@ -3,6 +3,8 @@ package br.com.compasso.springbootinterview.api.aplication.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -12,24 +14,28 @@ import br.com.compasso.springbootinterview.api.domain.cliente.ClienteRepository;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	public List<Cliente> ListAll() {
 		return clienteRepository.findAll();
 	}
-	
+
 	public Cliente findById(long id) {
 		Optional<Cliente> optionalCliente = clienteRepository.findById(id);
 		Cliente cliente = optionalCliente.orElseThrow(() -> new EmptyResultDataAccessException(1));
 		return cliente;
 	}
-	
+
 	public Cliente findByNome(String nome) {
 		Optional<Cliente> optionalCliente = Optional.ofNullable(clienteRepository.findByNome(nome));
 		Cliente cliente = optionalCliente.orElseThrow(() -> new EmptyResultDataAccessException(1));
 		return cliente;
+	}
+
+	public Cliente create(@Valid Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 
 }
--- spring-boot-interview/src/main/java/br/com/compasso/springbootinterview/api/infrastruture/web/controller/ClienteController.java
+++ spring-boot-interview/src/main/java/br/com/compasso/springbootinterview/api/infrastruture/web/controller/ClienteController.java
@@ -1,12 +1,16 @@
 package br.com.compasso.springbootinterview.api.infrastruture.web.controller;
 
 import java.util.List;
-import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.compasso.springbootinterview.api.aplication.service.ClienteService;
@@ -15,23 +19,29 @@ import br.com.compasso.springbootinterview.api.domain.cliente.Cliente;
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@GetMapping
 	public List<Cliente> ListAll() {
 		return clienteService.ListAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public Cliente findById(long id) {
 		return clienteService.findById(id);
 	}
-	
+
 	@GetMapping("/nome/{nome}")
 	public Cliente findByNome(String nome) {
 		return clienteService.findByNome(nome);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@Valid @RequestBody Cliente cliente) {
+		return clienteService.create(cliente);
 	}
 
 }
